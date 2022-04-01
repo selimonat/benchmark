839fb6fdb14d2859dea94c1c4e8eff6f8f0c4d0b
Selim Onat <selim.onat@mytaxi.com>
2022-04-01 22:45:35 +0200

    parser fills in missing action columns.

--- portfolio/Parser.py
+++ portfolio/Parser.py
@@ -9,6 +9,13 @@ pd.set_option('display.width', 1000)
 
 logger = utils.get_logger(__name__)
 
+mapping = {'price': ['purchase price'],
+           'action': [],
+           'quantity': ['quantity', 'amount'],
+           'ticker': ['symbol'],
+           'date': ['trade date', 'purchase date']
+           }
+
 
 def parse(filename):
     df = pd.read_csv(filename)
@@ -16,17 +23,20 @@ def parse(filename):
     df = df.rename(mapper=column_mapper, axis=1)
     # clean columns
     df = df.loc[:, df.columns[df.columns.isna() == False]]
+    # are all required columns present?
+    if len(set(mapping)) != len(set(df.columns)):
+        logger.info('Not all wanted columns there.')
+        missing = set(mapping) - set(df.columns)
+        logger.info(f'Missing is {missing}')
+        if missing == set(('action',)):
+            logger.info(f'Assuming missing actions as buy.')
+            df['action'] = 'buy'
     logger.debug('Parsed export file as follows\n' + df.to_string())
     return df
 
 
 def column_mapper(old_colname):
-    mapping = {'price': ['purchase price'],
-               'action': [],
-               'quantity': ['quantity', 'amount'],
-               'ticker': ['symbol'],
-               'date': ['trade date', 'purchase date']
-               }
+
     new_colname = [k for k, v in mapping.items() if old_colname.lower() in v]
     logger.debug(f'Mapping {old_colname} to {new_colname}')
 
@@ -37,4 +47,4 @@ def column_mapper(old_colname):
 
 
 if __name__ == '__main__':
-    df = parse(filename='examples/portfolio_01.csv')
+    df2 = parse(filename='examples/portfolio_01.csv')
