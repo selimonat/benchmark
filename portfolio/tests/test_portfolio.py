6608167f53b213a47929f1a3f18bd2a45d802ded
Selim Onat <selim.onat@mytaxi.com>
2022-04-04 19:32:36 +0200

    implementing portfolio tests.

--- portfolio/tests/test_portfolio.py
+++ portfolio/tests/test_portfolio.py
@@ -1,11 +1,17 @@
 from portfolio import utils
+from portfolio.Parser import parser
+SECONDS_IN_A_DAY = (60*60*24)
+logger = utils.get_logger(__name__)
+
 
 def test_return_integer():
     # distance between Start and stop times should return an integer when divided by number of seconds in a day.
     # step size check.
-    pass
 
-    # (utils.today() - parser.parse("20200101T000000 UTC").timestamp()) /  (60*60*24)
+    d = (utils.today() - parser.parse("20200101T000000 UTC").timestamp()) / SECONDS_IN_A_DAY
+    logger.info(f"Number of seconds between any time points must be a multiple of number of seconds in a day. Found "
+                f"a multiple which is equals to {d}.")
+    assert d == round(d)
 
 #  TODO: date columns must be string
 #  TODO: asset return at purchase date must be 100%
