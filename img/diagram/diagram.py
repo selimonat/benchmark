be5274bfd8f659ef1304be22db2b6d1199dadcd4
Selim Onat <selim.onat@mytaxi.com>
2022-04-07 16:56:12 +0200

    diagram updated to show classes.

--- img/diagram/diagram.py
+++ img/diagram/diagram.py
@@ -1,12 +1,19 @@
 from diagrams import Cluster, Diagram
 from diagrams.elastic.elasticsearch import Elasticsearch, Kibana
 from diagrams.programming.language import Python
+from diagrams.generic.storage import Storage
 
 
 with Diagram("Event processing", show=False):
-    with Cluster("Containers"):
+    connector = Python("DB connector")
+    portfolio = Python("Portfolio")
+    parser = Python("Parser")
+    file = Storage("Portfolio Export")
+    with Cluster("Services"):
         downloader = Python("Downloader")
         es = Elasticsearch("ES")
         kibana = Kibana('Monitoring')
 
-    downloader >> es << kibana
+    downloader >> connector >> es << kibana
+    file >> parser << connector << es
+    portfolio << parser
