0df637d53ccee3a1c211a9bfc725c557a0e0a5ec
Selim Onat <selim.onat@mytaxi.com>
2022-04-08 11:39:32 +0200

    Made a class out of file parser and added the possibility to return a list of positions as a representation of the transaction table. this was previously implemented in the Portfolio.py.

--- portfolio/Parser.py
+++ portfolio/Parser.py
@@ -2,96 +2,107 @@
 import pandas as pd
 import portfolio.utils as utils
 from dateutil import parser
-from typing import AnyStr
+from typing import AnyStr, List
+from portfolio.Position import Position
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
-logger = utils.get_logger(__name__)
-
-# used to map all columns names listed in values, to their keys.
+# used to map all column names listed in values, to their keys {target column name: export file column name}.
+# the column names listed as values are encountered on the .csv file and then mapped to final column names (shown in
+# keys).
 mapping = {'price': ['purchase price'],
-           'action': [],
+           'action': ['position'],
            'quantity': ['quantity', 'amount'],
-           'ticker': ['symbol'],
+           'ticker': ['symbol', 'ticker'],
            'date_human': ['trade date', 'purchase date']
            }
 
 
-def parse_file(filename: AnyStr) -> pd.DataFrame:
-    """
-    Extracts 5 columns from the export: price, action, quantity, ticker and date. If action not present, assumes buy.
-    Args:
-        filename:
+class PortfolioParser:
 
-    Returns:
-        df: parsed data structure as pandas dataframe with the following structure:
-            df.index (categorical):
-            df.action (categorical):
-            df.price (float):
-            df.quantity (float):
-            df.ticker (categorical):
-    """
-    df = pd.read_csv(filename)
-    # rename columns
-    df = df.rename(mapper=column_mapper, axis=1)
-    # clean columns
-    df = df.loc[:, df.columns[df.columns.isna() == False]]
-    # are all required columns present?
-    if len(set(mapping)) != len(set(df.columns)):
-        logger.info('Not all wanted columns there.')
-        missing = set(mapping) - set(df.columns)
-        logger.info(f'Missing is {missing}')
-        # Column completion:
-        if missing == set(('action',)):
-            logger.info(f'Assuming missing actions as buy.')
-            df['action'] = 'buy'
+    def __init__(self, filename):
+        self.logger = utils.get_logger(__name__)
+        self.filename = filename
+        self.df = self.parse_file(self.filename)
 
-        if (set(mapping) - set(df.columns)) == set():
-            logger.info("No more missing columns")
-            # TODO: Test if the exception is correctly raised.
-            raise Exception(f'Cannot continue with missing incomplete columns list:\n {df.columns}')
+    @property
+    def positions(self) -> List:
+        """
+        Returns: Returns parsed .csv export file as a list of positions.
+        """
+        def callback(col) -> Position:
+            return Position(col.action, col.quantity, col.ticker, col.date)
 
-    logger.debug('Parsed export file as follows\n' + df.to_string())
-    logger.info('Parsing date column.')
-    df['date'] = df['date_human'].astype(str).apply(parser.parse).astype(int) / 10 ** 9
-    df['date'] = df['date'].astype(int)
-    logger.info('Sorting transaction table by date')
-    df.sort_values(by='date', inplace=True)
-    logger.debug('Converted, now it looks like this\n' + df.to_string())
-    logger.info('Adjusting datatypes, avoiding objects.')
-    df['action'] = df['action'].astype("category")
-    df['ticker'] = df['ticker'].astype("category")
-    df['price'] = df['price'].astype(float)
-    # TODO: validity check: if the returned value is NaN, then it is possible that this was a weekend or so. which
-    #  would lead NaN to be returned for the price.
-    return df
+        # noinspection PyTypeChecker
+        return self.df.T.apply(callback).to_list()
 
+    def parse_file(self, filename: AnyStr) -> pd.DataFrame:
+        """
+        Extracts 5 columns from the export: price, action, quantity, ticker and date. If action not present, assumes buy.
+        Args:
+            filename:
 
-def column_mapper(old_colname):
-    """
-    Maps column names as specified in the mapping variable. Standardize column names by mapping similar ones to the
-    same one e.g. Purchase Price -> price; buy Price -> price, etc.
-    It is called by the rename method of pandas.
-    Args:
-        old_colname:
-    Returns:
-        new column name, which are
-    """
+        Returns:
+            df: parsed data structure as pandas dataframe with the following structure:
+                df.index (categorical):
+                df.action (categorical):
+                df.price (float):
+                df.quantity (float):
+                df.ticker (categorical):
+        """
+        df = pd.read_csv(filename)
+        # rename columns
+        df = df.rename(mapper=self.column_mapper, axis=1)
+        # clean columns
+        df = df.loc[:, df.columns[df.columns.isna() == False]]
+        # are all required columns present?
+        if len(set(mapping)) != len(set(df.columns)):
+            self.logger.info('Not all wanted columns there.')
+            missing = set(mapping) - set(df.columns)
+            self.logger.info(f'Missing is {missing}')
+            # Column completion:
+            if missing == set(('action',)):
+                self.logger.info(f'Assuming missing actions as buy.')
+                df['action'] = 'buy'
 
-    new_colname = [k for k, v in mapping.items() if old_colname.lower() in v]
-    logger.debug(f'Mapping {old_colname} to {new_colname}')
+            if (set(mapping) - set(df.columns)) != set():
+                self.logger.info("No more missing columns")
+                # TODO: Test if the exception is correctly raised.
+                raise Exception(f'Cannot continue with missing incomplete columns list:\n {df.columns}')
 
-    if not new_colname:
-        return None
-    else:
-        return new_colname[0]
+        self.logger.debug('Parsed export file as follows\n' + df.to_string())
+        self.logger.info('Parsing date column.')
+        df['date'] = df['date_human'].astype(str).apply(parser.parse).astype(int) / 10 ** 9
+        df['date'] = df['date'].astype(int)
+        self.logger.info('Sorting transaction table by date')
+        df.sort_values(by='date', inplace=True)
+        self.logger.debug('Converted, now it looks like this\n' + df.to_string())
+        self.logger.info('Adjusting datatypes, avoiding objects.')
+        df['action'] = df['action'].astype("category")
+        df['ticker'] = df['ticker'].astype("category")
+        df['price'] = df['price'].astype(float)
+        # TODO: validity check: if the returned value is NaN, then it is possible that this was a weekend or so. which
+        #  would lead NaN to be returned for the price.
+        return df
 
+    def column_mapper(self, old_colname):
+        """
+        Maps column names as specified in the mapping variable. Standardize column names by mapping similar ones to the
+        same one e.g. Purchase Price -> price; buy Price -> price, etc.
+        It is called by the rename method of pandas.
+        Args:
+            old_colname:
+        Returns:
+            new column name, which are
+        """
 
-if __name__ == '__main__':
-    df2 = parse_file(filename='examples/portfolio_02.csv')
-    from portfolio.Portfolio import Portfolio
-    p = Portfolio(df2)
+        new_colname = [k for k, v in mapping.items() if old_colname.lower() in v]
+        self.logger.debug(f'Mapping {old_colname} to {new_colname}')
 
+        if not new_colname:
+            return None
+        else:
+            return new_colname[0]
--- portfolio/tests/test_parser.py
+++ portfolio/tests/test_parser.py
@@ -1,10 +1,12 @@
-from portfolio.Parser import parse_file
+from portfolio.Parser import PortfolioParser
+from portfolio.Position import Position
 import pandas as pd
 
 
 def test_transaction_table_data_types():
-
-    df = parse_file(filename='../examples/portfolio_01.csv')
+    filename = '../examples/portfolio_01.csv'
+    pp = PortfolioParser(filename)
+    df = pp.df
 
     assert pd.api.types.is_categorical_dtype(df.action)
     assert pd.api.types.is_categorical_dtype(df.ticker)
@@ -12,3 +14,9 @@ def test_transaction_table_data_types():
     assert pd.api.types.is_int64_dtype(df.date)
     assert pd.api.types.is_int64_dtype(df.date_human)
     assert pd.api.types.is_float_dtype(df.price)
+
+
+def test_position_output():
+    filename = '../examples/portfolio_01.csv'
+    pp = PortfolioParser(filename)
+    assert type(pp.positions) == Position
