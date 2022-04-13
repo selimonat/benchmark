90735c523f0c41b9e1628c92045854f168475a42
Selim Onat <selim.onat@mytaxi.com>
2022-04-13 17:40:37 +0200

    new portfolio object computing returns.

--- portfolio/Portfolio.py
+++ portfolio/Portfolio.py
@@ -1,59 +1,8 @@
-from portfolio import utils
-from portfolio import Database
+from portfolio.Database import DB
+from portfolio.Ticker import Ticker
+from typing import List
 import pandas as pd
-from dateutil import parser
-import numpy as np
-from typing import SupportsFloat
-
-db = Database.DB()
-
-
-class Position:
-    """
-    Intermediate data structure between parsed export file and transaction table. If purchase price not present,
-    it adds one. Represents the features associated with a single position.
-    """
-
-    def __init__(self, action: str, quantity: int, ticker: str, date: int):
-        self.logger = utils.get_logger(__name__)
-        self.action = action
-        self.quantity = quantity
-        self.ticker = ticker
-        self.date = date
-        self.price = self.value_at([self.date])
-
-    @property
-    def df(self) -> pd.DataFrame:
-        """
-        Returns:
-            DataFrame: 1 row pandas view on the position with attributes as columns.
-        """
-        out = pd.DataFrame.from_dict([self.__dict__])
-        # conversion of data types
-        out.action = out.action.astype('category')
-        out.quantity = out.quantity.astype(float)
-        out.ticker = out.ticker.astype('category')
-        out.date = out.date.astype(int)
-        out.price = out.price.astype(float)
-        return out
-
-    def value_at(self, date: list) -> SupportsFloat:
-        """
-        Value at an arbitrary date.
-        Args:
-            date: epoch seconds
-
-        Returns:
-            list: The value of the position. Returns NaN if no value.
-        """
-        out = db.read(ticker=self.ticker, date=date, output_format='series')
-        if out.shape[0] == 1:
-            return out.iloc[0]
-        elif out.shape[0] == 0:
-            self.logger.info(f"No value found for {self.ticker} at {self.date}, returning NaN")
-            return float('nan')
-        else:
-            raise Exception("The output should only be at most of length 1")
+db = DB()
 
 
 class Portfolio:
@@ -62,120 +11,18 @@ class Portfolio:
     The class instance generates first a transaction table (indexed on transaction id) from the parsed export file.
     """
 
-    def __init__(self, df):
+    def __init__(self, tickers: List[Ticker]):
         """
 
         Args:
-            df: dataframe resulting from parsing the export file.
+            tickers: (Ticker) da`taframe resulting from parsing the export file.
         """
-        actions = df.action
-        quantities = df.quantity
-        tickers = df.ticker
-        dates = df.date
-
-        out = [Position(act, am, t, d).df for act, am, t, d in zip(actions, quantities, tickers, dates)]
-
-        df = pd.concat(out, axis=0).reset_index(drop=True)
-        # assign a lot id to each transaction
-        self.table_transaction = utils.group_rank_id(df, 'ticker', ['ticker', 'date']).rename(columns={'rank': 'lot'})
-        self.table_transaction['lot'] = self.table_transaction['lot'] + 1
+        self.tickers = tickers
 
     @property
-    def table_time_course_asset_price(self):
-        """
-        This table expands the transaction table in time to represent time-course of asset prices. Every single asset
-        that is bought is represented as a separate column.
-        Returns:
-            df: organized as [time, asset-names].
-        """
-
-        def expander_asset_price(col: pd.Series):
-            """Expands each row in transaction table across time where each time point represents the market price of an
-            asset.
-            """
-            ticker = col.ticker
-            #  TODO: it is not guaranteed that the returned data will be perfectly indexed on self.time_line.
-            return db.read(ticker, date=self.time_line, output_format='series')
-
-        #  need this intermediate variable to have correct column names after apply(), otherwise they follow the
-        #  indices of the transaction table.
-        out = self.table_transaction.copy().T
-        out.columns = out.loc['ticker'].values
-        # transaction table => Expand in time
-        return out.apply(expander_asset_price).groupby(level=0, axis=1).mean()
-
-    @property
-    def table_time_course_asset_quantity(self):
-        """
-        This table expands the transaction table to keep track of the number of assets purchased at any given time
-        point. It is aligned with the table_asset_time_course.
-        Returns:
-
-        """
-
-        def expander_asset_quantity(col: pd.Series):
-            """Expands transaction table across time where each time point represents the cost of assets"""
-            ticker = col.ticker
-            # This should be 0 before the purchase date.
-            s = pd.Series(0,
-                          index=pd.Index(self.time_line, name='time'),
-                          name=ticker)
-
-            s.loc[s.index >= col.date] = col.quantity
-            return s
-
-        out = self.table_transaction.copy().T
-        out.columns = out.loc['ticker'].values
-        return out.apply(expander_asset_quantity).groupby(level=0, axis=1).sum()
-
-    @property
-    def table_time_course_asset_cost(self):
-        def expander_asset_cost(col: pd.Series):
-            """Expands transaction table across time where each time point represents the number of own assets"""
-            ticker = col.ticker
-            return pd.Series(col.price,
-                             index=pd.Index(self.time_line, name='time'),
-                             name=ticker)
-
-        out = self.table_transaction.copy().T
-        out.columns = out.loc['ticker'].values
-        return out.apply(expander_asset_cost).groupby(level=0, axis=1).mean()
-
-    @property
-    def table_time_course_asset_returns(self):
-        return self.table_time_course_asset_price / self.table_time_course_asset_cost * 100
-
-    # @property
-    # def table_time_course_portfolio_returns(self):
-    #     pass
-    # return self.table_time_course_asset_returns
-
-    @property
-    def start_time(self):
-        # Start time of the portfolio, typically earliest opened position
-        return self.table_transaction.date.min()
-
-    @property
-    def current_time(self):
-        # Returns current day as timestamp in UTC
-        return utils.today()
-
-    @property
-    def time_line(self):
-        # The time axis of the portfolio. step size is day.
-        return np.arange(self.start_time, self.current_time, (60 * 60 * 24), dtype=int)
-
     def returns(self):
-        pass
-
-    @staticmethod
-    def plot(s: pd.Series):
-        from uniplot import plot
-        valid_index = ~s.T.isna()
-        x = s.index.values[valid_index] / (60*60*24*365) + 1970
-        y = s.T.values[valid_index]
-        plot(xs=x,
-             ys=y,
-             lines=True,
-             x_unit='date',
-             width=150)
+        a = pd.concat([t.returns for t in self.tickers], axis=1)
+        print(a)
+        w = pd.concat([t.total_invested for t in self.tickers], axis=1)
+        print(w)
+        return (a * w).sum(axis=1) / w.sum(axis=1)
--- portfolio/tests/test_portfolio.py
+++ portfolio/tests/test_portfolio.py
@@ -1,8 +1,10 @@
 from portfolio import utils
-from portfolio.Parser import parse_file
+from portfolio.Parser import PortfolioParser
 from dateutil.parser import parse
 import pandas as pd
-from portfolio.Portfolio import Portfolio, Position
+from portfolio.Portfolio import Portfolio
+from portfolio.Position import Position
+from portfolio.Ticker import Ticker
 from portfolio import Database
 import numpy as np
 
@@ -22,105 +24,36 @@ def test_return_integer():
     assert d == round(d)
 
 
-def test_ticker_column_must_be_categorical():
-    # dtype check.
-    # Avoid object dtypes as they are slow.
-    df = parse_file(filename='../examples/portfolio_02.csv')
-    assert isinstance(df['ticker'].dtype, pd.api.types.CategoricalDtype)
+def test_balanced():
+    quantity1, cost1 = 1, 100
+    quantity2, cost2 = 1, 100
+    pos1 = Position(action='buy', quantity=quantity1, ticker='FB', date=utils.today(), cost=cost1)
+    pos2 = Position(action='buy', quantity=quantity1, ticker='GOOG', date=utils.today(), cost=cost1)
+    value = 200
+    tickers = [Ticker([pos1], value=value), Ticker([pos2], value=value)]
+
+    p = Portfolio(tickers)
+
+    r1 = 100 * (value * quantity1 - cost1 * quantity1) / (cost1 * quantity1)
+    r2 = 100 * (value * quantity2 - cost2 * quantity2) / (cost2 * quantity2)
+    r = (r1 * cost1 * quantity1 + r2 * cost2 * quantity2) / (cost1 * quantity1 + cost2 * quantity2)
+
+    assert p.returns.values == r
 
 
-def test_action_column_must_be_categorical():
-    # dtype check for the parsed transaction table.
-    # Avoid object dtypes as they are slow.
-    df = parse_file(filename='../examples/portfolio_02.csv')
-    assert pd.api.types.is_categorical_dtype(df.action)
-    assert pd.api.types.is_categorical_dtype(df.ticker)
+def test_unbalanced():
+    quantity1, cost1 = 1, 100
+    quantity2, cost2 = 10, 50
+    value = 200
+    pos1 = Position(action='buy', quantity=quantity1, ticker='FB', date=utils.today(), cost=cost1)
+    pos2 = Position(action='buy', quantity=quantity2, ticker='GOOG', date=utils.today(), cost=cost2)
 
+    tickers = [Ticker([pos1], value=value), Ticker([pos2], value=value)]
 
-def test_position_data_types():
-    pos = Position('buy', 4, 'AAPL', 377481600)
-    assert pd.api.types.is_categorical_dtype(pos.df.action)
-    assert pd.api.types.is_categorical_dtype(pos.df.ticker)
-    assert pd.api.types.is_float_dtype(pos.df.quantity) or pd.api.types.is_int64_dtype(pos.df.quantity)
-    assert pd.api.types.is_int64_dtype(pos.df.date)
-    assert pd.api.types.is_float_dtype(pos.df.price)
+    p = Portfolio(tickers)
 
+    r1 = 100 * (value - cost1) / cost1
+    r2 = 100 * (value - cost2) / cost2
+    r = (r1 * cost1 * quantity1 + r2 * cost2 * quantity2) / (cost1 * quantity1 + cost2 * quantity2)
 
-def test_is_index_sorted_in_time_course_table():
-    df = parse_file(filename='../examples/portfolio_01.csv')
-    p = Portfolio(df)
-    assert p.table_time_course_asset_price.index.is_monotonic
-
-
-def test_no_nan_rows_in_time_course_tables():
-    # There must never be any single row which is full of nans. At least one column must have a non-nan value.
-    df = parse_file(filename='../examples/portfolio_01.csv')
-    p = Portfolio(df)
-    logger.info(f"There should be no row with only nan values.")
-    assert p.table_time_course_asset_price.isna().sum(axis=1).max() < p.table_time_course_asset_price.shape[1]
-
-
-def test_lot_numbers():
-    # When the same ticker is entered N times, the max available lot number should be equal to N.
-    df = parse_file(filename='../examples/portfolio_03.csv')
-    p = Portfolio(df)
-    a = p.table_transaction.groupby('ticker').count()['quantity'].to_frame()
-    b = p.table_transaction.groupby('ticker').max()['lot'].to_frame()
-    df = pd.DataFrame.join(a, b)
-    assert df['quantity'].equals(df['lot'])
-
-
-def test_asset_price():
-    # As the database is currently generating random numbers, this will fail.
-    logger.info(f"If we randomly select an entry from the asset price time-series, it should match the value in the "
-                f"database.")
-    df = parse_file(filename='../examples/portfolio_03.csv')
-    p = Portfolio(df)
-    out = p.table_time_course_asset_price.unstack(-1)
-    out = out.loc[out.isna() == False].sample(1).reset_index()
-    s = db.read(out['level_0'].values[0], out['date'].to_list(), output_format='series')
-    assert s.iloc[0] == out[0].values[0]
-
-
-def test_time_course_tables():
-    #  simple test to check if time-course tables run at all.
-    df = parse_file(filename='../examples/portfolio_03.csv')
-    p = Portfolio(df)
-    assert type(p.table_time_course_asset_price) == pd.DataFrame
-    assert type(p.table_time_course_asset_cost) == pd.DataFrame
-    assert type(p.table_time_course_asset_returns) == pd.DataFrame
-    assert type(p.table_time_course_asset_quantity) == pd.DataFrame
-
-
-def test_return_at_purchase_date():
-    # return at purchase date must be 100%
-    df = parse_file(filename='../examples/portfolio_01.csv')
-    p = Portfolio(df)
-    for i in p.table_transaction[["ticker", "date"]].to_dict(orient='records'):
-        print(i)
-        df = p.table_time_course_asset_returns
-        assert df.loc[i['date'], i['ticker']] == 100.
-
-
-def test_check_quantity_at_date_of_purchase():
-    df = parse_file(filename='../examples/portfolio_03.csv')
-    p = Portfolio(df)
-    df = p.table_time_course_asset_quantity
-    for ticker in p.table_transaction["ticker"].unique():
-        # transaction dates
-        rows = p.table_transaction['ticker'] == ticker
-        transaction_dates = p.table_transaction.loc[rows, 'date']
-        q_time_course = df.loc[transaction_dates, ticker]
-
-        q_transaction_table = p.table_transaction.loc[rows, 'quantity'].cumsum()
-        assert np.all(q_transaction_table.values == q_time_course.values)
-# def test_check_quantity_at_today():
-#     # The amount of stocks owned must be +s of buys and -s of sells
-#     df = parse_file(filename='../examples/portfolio_03.csv')
-#     p = Portfolio(df)
-#     1
-
-#  TODO: asset return at purchase date must be 100%
-#  TODO: Transaction table must have indices from 0 to the shape[0] of the table.
-#  TODO: Two times the same ticker must result in two different columns in the time-course table.
-#  TODO: When same ticker added twice the asset price must be the mean of them
+    assert p.returns.values == r
