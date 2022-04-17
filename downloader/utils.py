5f5d5ac367b3e7774019eef683bd406ad4adb31a
Selim Onat <selim.onat@mytaxi.com>
2022-04-17 19:25:59 +0200

    caching, error handling implemented.

--- downloader/utils.py
+++ downloader/utils.py
@@ -1,18 +1,48 @@
-import pandas as pd
 import logging
 import os
+from typing import Union
+from urllib.error import URLError
+
+import pandas as pd
 
 
-def get_all_tickers():
+def get_all_tickers() -> Union[pd.Series, None]:
     """
     Gets all tickers listed in the Nasdaq exchange from their public ftp server.
-    :return: symbol list as a pandas series.
+
+    Returns:
+        (None or Series): A pandas series of Nasdaq listed tickers.
     """
-    # https://www.nasdaqtrader.com/Trader.aspx?id=SymbolDirDefs
-    url = 'ftp://ftp.nasdaqtrader.com/SymbolDirectory/nasdaqlisted.txt'
+
+    # an alternative ticker list, maybe be merge this with the one above.
     # url = 'ftp://ftp.nasdaqtrader.com/SymbolDirectory/otherlisted.txt'
-    df = pd.read_csv(url, sep='|', skipfooter=1)
-    return df['Symbol']
+    cached_file = './downloader/tickers/nasdaq.csv'
+    url = 'ftp://ftp.nasdaqtrader.com/SymbolDirectory/nasdaqlisted.txt'
+    logger = get_logger('get_all_tickers')
+    try:
+        logger.info('Connecting to Nasdaq servers to fetch ticker list.')
+        df = pd.read_csv(url, sep='|', skipfooter=1)
+        # df can still be returned empty
+        if df.empty:
+            logger.info("Returned dataframe is empty.")
+            raise ValueError('Returned dataframe is empty')
+
+        logger.info(f"Found DataFrame: {df.head(10)}")
+        logger.info(f'Caching the list to local disk as {cached_file}.')
+        df.to_csv(cached_file)
+        return df['Symbol']
+
+    except (URLError, ValueError) as error:
+        logging.error(error)
+        logging.error(f'URL {url}')
+        logging.warning("Attempt to load cached tickers.")
+        if os.path.exists(cached_file):
+            df = pd.read_csv(cached_file)
+            logging.warning(f"Seemed to work, found df with {df.shape[0]} rows.")
+            return df['Symbol']
+        else:
+            logging.warning('Cached file doesnt exist.')
+            return None
 
 
 def get_logger(name):
