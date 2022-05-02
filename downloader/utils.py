27e68e1560fff9c213c236b63c9d5720ff1c2481
Selim Onat <selim.onat@mytaxi.com>
2022-05-02 13:44:32 +0200

    removed code duplication. logger is now important from main utils.

--- downloader/utils.py
+++ downloader/utils.py
@@ -4,8 +4,11 @@ from typing import Union, AnyStr
 import datetime
 from urllib.error import URLError
 import yfinance as yf
+from portfolio.utils import get_logger
 
 import pandas as pd
+
+logger = get_logger(__name__)
 
 
 def get_all_tickers() -> Union[pd.Series, None]:
@@ -20,7 +23,6 @@ def get_all_tickers() -> Union[pd.Series, None]:
     # url = 'ftp://ftp.nasdaqtrader.com/SymbolDirectory/otherlisted.txt'
     cached_file = './benchmark/downloader/tickers/nasdaq.csv'
     url = 'ftp://ftp.nasdaqtrader.com/SymbolDirectory/nasdaqlisted.txt'
-    logger = get_logger('get_all_tickers')
     try:
         logger.info('Connecting to Nasdaq servers to fetch ticker list.')
         df = pd.read_csv(url, sep='|', skipfooter=1)
@@ -47,29 +49,6 @@ def get_all_tickers() -> Union[pd.Series, None]:
             return None
 
 
-def get_logger(name):
-    # create logger
-    logger_ = logging.getLogger(name)
-    logger_.setLevel(logging.DEBUG)
-    # create formatter
-    fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    formatter = logging.Formatter(fmt)
-    # create console handler and set level to debug and add formatter to ch
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-    ch.setFormatter(formatter)
-    # add ch to logger
-    logger_.addHandler(ch)
-    # do the same with a file handler.
-    if not os.path.exists('log'):
-        os.makedirs('log')
-    f_handler = logging.FileHandler(os.path.join('log', f'{name}.log'))
-    f_handler.setLevel(logging.DEBUG)
-    f_handler.setFormatter(formatter)
-    logger_.addHandler(f_handler)
-    return logger_
-
-
 def yf_call(ticker: AnyStr,
             start: datetime = datetime.datetime.strptime('1900-01-01', '%Y-%m-%d')) -> pd.DataFrame:
     """
@@ -80,7 +59,6 @@ def yf_call(ticker: AnyStr,
     Returns:
         dataframe:  a standard DF with Close, ticker columns indexed on date.
     """
-    logger = get_logger(__name__)
 
     logger.info(f"Making a direct YF call for ticker: {ticker} and start_date: {start}")
     t = yf.Ticker(ticker)
